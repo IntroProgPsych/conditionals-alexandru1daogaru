ab19dd028607f8bfdc9be2268ebddee1df19bbb3
alexandru1daogaru <alexandru.daogaru06@e-uvt.ro>
2025-11-20 09:53:14 +0000

    j

--- if.py
+++
@@ -1,9 +0,0 @@
-age = int(input("Type your age"))
-if age>=18 and age<100:
-    print("You are an adult")
-elif age>=100:
-    print("You are too old boy")
-elif age<0:
-    print("you are not born yet")
-else:
-    print("You are a child")
