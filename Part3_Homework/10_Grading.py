87f75483c292702174768f645ca47733fb565b66
alexandru1daogaru <alexandru.daogaru06@e-uvt.ro>
2025-11-20 09:52:22 +0000

    jh

--- Part3_Homework/10_Grading.py
+++ Part3_Homework/10_Grading.py
@@ -1,3 +1,4 @@
+
 # You are tasked with creating a simple grading system for a class. 
 # Write a Python program that takes a student's score as input
 #  and calculates and prints its corresponding letter grade based on the following grading scale:
@@ -7,3 +8,4 @@
 # 70-79: C
 # 60-69: D
 # Below 60: F
+
